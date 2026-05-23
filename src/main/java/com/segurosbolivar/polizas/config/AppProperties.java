780d9f7807c465b2024b712b47a4e4d0061d9705
mend1001 <mend1001mend1001@gmail.com>
2026-05-23 15:20:28 -0500

    chore(config): agrega AppProperties y configura application.yml

---
+++ src/main/java/com/segurosbolivar/polizas/config/AppProperties.java
@@ -0,0 +1,15 @@
+package com.segurosbolivar.polizas.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app")
+@Getter
+@Setter
+public class AppProperties {
+
+    private String apiKey;
+    private String coreMockUrl;
+    private Double ipcDefault;
+}
