ce2f94bc949c960f9833bb7e39a2b3f230b10247
mend1001 <mend1001mend1001@gmail.com>
2026-05-23 18:03:33 -0500

    feat(service): actualiza Strategy validators para usar getName() en lugar de enum

--- src/main/java/com/segurosbolivar/polizas/service/validation/impl/AgregarRiskValidation.java
+++ src/main/java/com/segurosbolivar/polizas/service/validation/impl/AgregarRiskValidation.java
@@ -2,7 +2,6 @@ package com.segurosbolivar.polizas.service.validation.impl;
 
 import com.segurosbolivar.polizas.exception.BusinessException;
 import com.segurosbolivar.polizas.model.Policy;
-import com.segurosbolivar.polizas.model.enums.PolicyType;
 import com.segurosbolivar.polizas.service.validation.PolicyValidationStrategy;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -14,7 +13,7 @@ public class AgregarRiskValidation implements PolicyValidationStrategy {
 
     @Override
     public void validate(Policy policy) {
-        if (!PolicyType.COLECTIVA.equals(policy.getTipo())) {
+        if (!"COLECTIVA".equals(policy.getType().getName())) {
             throw new BusinessException(MSG_SOLO_COLECTIVA, HttpStatus.BAD_REQUEST);
         }
     }
--- src/main/java/com/segurosbolivar/polizas/service/validation/impl/RenovarPolicyValidation.java
+++ src/main/java/com/segurosbolivar/polizas/service/validation/impl/RenovarPolicyValidation.java
@@ -2,7 +2,6 @@ package com.segurosbolivar.polizas.service.validation.impl;
 
 import com.segurosbolivar.polizas.exception.BusinessException;
 import com.segurosbolivar.polizas.model.Policy;
-import com.segurosbolivar.polizas.model.enums.PolicyState;
 import com.segurosbolivar.polizas.service.validation.PolicyValidationStrategy;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -14,7 +13,7 @@ public class RenovarPolicyValidation implements PolicyValidationStrategy {
 
     @Override
     public void validate(Policy policy) {
-        if (PolicyState.CANCELADA.equals(policy.getEstado())) {
+        if ("CANCELADA".equals(policy.getState().getName())) {
             throw new BusinessException(MSG_NO_RENOVAR_CANCELADA, HttpStatus.BAD_REQUEST);
         }
     }
